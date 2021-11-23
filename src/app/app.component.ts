789310f20ccfac945d4cf8bc226c04a50919f074
[fdm89] <[fulvio.dimaio1@gmail.com]>
2021-11-23 18:02:14 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'parte-seconda-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'parte-seconda';
+}
